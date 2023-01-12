8b1767e53b3dfd90f2d9513d2b3046684cebab71
Jason <jasonetienne1@gmail.com>
2023-01-12 11:16:15 +0000

    edited my form.py file

--- main/inventory_component/forms.py
+++ main/inventory_component/forms.py
@@ -5,24 +5,24 @@ from .models import Inventory
 
 
 
-#USE THIS IF WANT TO ADD MORE CONDITIONS TO WHAT CAN BE ENTERED I.E TITLE MUST BE iTEM
+
 class ItemForm(forms.ModelForm):
     class Meta:
         model = Inventory
-        fields = ('item', 'description', 'quantity')
-        # If I want to update the labels for  Item, Description, Quantity
+        fields = ('item', 'description', 'quantity')        
         widgets = {
             'item': forms.TextInput(attrs={'class': 'form-control my-3'}),
             'description': forms.Textarea(attrs={'class': 'form-control my-3'}),
-            'item': forms.TextInput(attrs={'class': 'form-control my-3'})
+            'quantity': forms.TextInput(attrs={'class': 'form-control my-3'})
         }
+        # If I want to update the labels for  Item, Description, Quantity
         labels = {
             'item': 'Item Name',
             'description': 'Item Description',
             'quantity': 'Item Quantity'
         }
 '''
-# USE THIS IF WANT TO ADD SOME ERROR MESSAGE AND CONTROL WHAT WILL BE ACCEPTED IN AN INPUT FIELD
+# USE THIS IF WANT TO ADD SOME ERROR MESSAGE AND CONTROL WHAT WILL BE ACCEPTED IN AN INPUT FIELD I.E TITLE MUST BE iTEM
     def clean_title(self):
         item = self.cleaned_data('item')
         if 'Item name' not in item

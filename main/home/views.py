2a695bb6fbd436a7d9455549b00fc47dff1902b0
Jason <jasonetienne1@gmail.com>
2023-01-16 19:16:08 +0000

    added ADD button and tided up code

--- main/home/views.py
+++ main/home/views.py
@@ -11,6 +11,7 @@ from django.views.generic import TemplateView
 
 class HomePage(LoginRequiredMixin, TemplateView):
     template_name = 'home.html'
+    login_url = '/login'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)

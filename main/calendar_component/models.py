c4d57d5bcb1224870bdb3b468463218e208b93fb
Jason <jasonetienne1@gmail.com>
2023-03-01 10:02:16 +0000

    .

--- main/calendar_component/models.py
+++ main/calendar_component/models.py
@@ -1,34 +1,34 @@
-from django.db import models
-from django.urls import reverse
+from django.db import models 
+from django.urls import reverse 
 
+''' 
+class Booking(models.Model): 
+    title = models.CharField(max_length=200) 
+    description = models.TextField() 
+    start_time = models.DateTimeField() 
+    end_time = models.DateTimeField() 
+    completed = models.BooleanField(default=False) 
 '''
-class Booking(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
-    completed = models.BooleanField(default=False)
-'''
+ 
 
-
-
-class Booking(models.Model):
-    title = models.CharField(max_length=200)
-    details = models.TextField()
-    contact = models.CharField(max_length=200)
-    phone_number = models.CharField(max_length=15)    
-    quote = models.DecimalField(max_digits=5, decimal_places=2)    
-    materials_needed = models.TextField()
-    materials_cost = models.DecimalField(max_digits=5, decimal_places=2)
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
+ 
+class Booking(models.Model): 
+    title = models.CharField(max_length=200) 
+    details = models.TextField() 
+    contact = models.CharField(max_length=200) 
+    phone_number = models.CharField(max_length=15)     
+    quote = models.DecimalField(max_digits=5, decimal_places=2)     
+    materials_needed = models.TextField() 
+    materials_cost = models.DecimalField(max_digits=5, decimal_places=2) 
+    start_time = models.DateTimeField() 
+    end_time = models.DateTimeField() 
     completed = models.BooleanField(default=False) 
  
  
 
-    @property
-    def get_html_url(self):
-        url = reverse('calendar_component:event_edit', args=(self.id,))
+    @property 
+    def get_html_url(self): 
+        url = reverse('calendar_component:event_edit', args=(self.id,)) 
         return f'<a href="{url}"> {self.title} </a>' 
 
 

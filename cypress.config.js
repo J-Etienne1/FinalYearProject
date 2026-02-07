af817929eb260eaed8f210ae8ed1a30af7f8d657
Jason <58144689+J-Etienne1@users.noreply.github.com>
2026-02-07 22:37:11 +0000

    sfd

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,7 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    this.baseUrl = 'http://127.0.0.1:8000/'
     },
   },
 });

1514fa23e92d00386d86dfec1c5bb8c3df0c02bd
Jason <58144689+J-Etienne1@users.noreply.github.com>
2026-03-12 23:07:47 +0000

    kljndcnsdl

--- main/login_register_component/urls.py
+++ main/login_register_component/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.Login.as_view(), name='login'),
-    path('logout/', views.Logout.as_view(), name='logout'),
-    path('register/', views.Register.as_view(), name='register'),
+    path("", views.Login.as_view(), name="login"),
+    path("logout/", views.Logout.as_view(), name="logout"),
+    path("register/", views.Register.as_view(), name="register"),
 ]
--- main/login_register_component/views.py
+++ main/login_register_component/views.py
@@ -6,21 +6,17 @@ from django.views.generic import TemplateView
 
 # using djangos Loginview to handle user login
 class Login(LoginView):
-    template_name = 'login.html'
-
-
+    template_name = "login.html"
 
 
 # using djangos Logoutview to handle user logout
 class Logout(LogoutView):
-    template_name = 'logout.html'
-
-
+    template_name = "logout.html"
 
 
 # using djangos Createview to handle user registration and UserCreationForm for the registration form
 class Register(CreateView):
     form_class = UserCreationForm
-    template_name = 'register.html'
-    # redirect to login page after registration 
-    success_url = 'http://localhost:8000/login/'
+    template_name = "register.html"
+    # redirect to login page after registration
+    success_url = "http://localhost:8000/login/"
